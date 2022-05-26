0eb6127fbb39723081ef84361d68c6800a4c5d94
Yang Lin <97666098+ylf13@users.noreply.github.com>
2022-05-26 10:15:54 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+//  whale talk project
+
+//  Creating a variable named Input
+var input = "I am an aspiring UX UI designer.";
+
+// array named vowels
+const vowels = ["a", "e", "i", "o", "u"];
+
+// resultArray store the vowels from the input string
+resultArray = [];
+
+// iterate through each letter of the input variable text
+//  inner loop iterate through the vowels array each time the outer loop runs
+//  insdie inner loop - if statement compares the input letter to every letter in the vowels array
+for (let i = 0; count < input.length; i++) {
+  console.log(input);
+  if(input === "e") {
+    resultArray.push("i");
+  }
+  for (let j = 0; count < vowels.length; j++) {
+    console.log(vowels);
+    if(input === vowels)
+  }
+}
+
+console.log (resultArray);
+
+var resultString = resultArray.join()
